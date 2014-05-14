a83f0b169cb65c5054d6fda76bc5c5583456e630
KakuLance <gxlance@gmail.com>
2014-05-14 13:26:45 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClubBiz::Application.config.secret_key_base = '564a2237a524e2213c372fe0ffcc33adf46b37e60c0b3e16b3c8828fbf53e70c6ad6881f8a806a43763a1ad3251f476a1aff4333a063efb27e0907b2e869e745'
